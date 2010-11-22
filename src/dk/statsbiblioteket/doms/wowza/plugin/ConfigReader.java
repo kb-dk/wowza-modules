d89060d528f67647852ae9a3487b865e15431e79
Mads Ravn <mar@statsbiblioteket.dk>
2010-11-22 15:42:52 +0000

    Changed the ConfigReader

--- src/dk/statsbiblioteket/doms/wowza/plugin/ConfigReader.java
+++ src/dk/statsbiblioteket/doms/wowza/plugin/ConfigReader.java
@@ -1,6 +1,9 @@
 package dk.statsbiblioteket.doms.wowza.plugin;
 
-import java.util.HashMap;
+
+
+
+import java.util.Properties;
 import java.io.*;
 
 /**
@@ -9,7 +12,7 @@ import java.io.*;
 public class ConfigReader {
 
 
-    private HashMap<String, String> hashmap;
+    private Properties properties;
 
     /**
      * TODO javadoc
@@ -17,38 +20,10 @@ public class ConfigReader {
      */
     public ConfigReader(String path) {
 
-        hashmap = new HashMap<String, String>();
-        File file = new File(path);
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        BufferedReader dis = null;
-        HashMap<String,String> hashmap = new HashMap<String, String>();
+        properties = new Properties();
         try {
-            fis = new FileInputStream(file);
-
-            bis = new BufferedInputStream(fis);
-            dis = new BufferedReader(new InputStreamReader(bis));
-            String s = "";
-            while (dis.ready()) {
-
-                s=dis.readLine().trim();
-                String t =  s.substring(s.indexOf("<")+1,s.indexOf(">"));
-
-                String u = s.substring(s.indexOf("<" + t + ">")+t.length()+2, s.indexOf("</"+t+">"));
-                hashmap.put(t,u);
-
-
-
-            }
-
-            fis.close();
-            bis.close();
-            dis.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            properties.load(new FileInputStream(path));
+        } catch (IOException e) { System.err.println(e); }
 
     }
 
@@ -58,6 +33,10 @@ public class ConfigReader {
      * @return object of key
      */
     public String get(String key) {
-        return hashmap.get(key);
+        return properties.getProperty(key);
+    }
+
+    public String get(String key, String defaultValue) {
+        return properties.getProperty(key,defaultValue);
     }
 }
