21f3c8e6336c9b3b0b2f1121506c9e8666c1ec9f
Henning Böttger <heb@statsbiblioteket.dk>
2011-03-28 10:30:19 +0000

    Utility for generating tickets and connections strings for Wowzas simplestreamingclient

--- trunk/scripts/getTicket.sh
+++ trunk/scripts/getTicket.sh
@@ -3,7 +3,8 @@
 TICKET_SERVER=http://alhena:7880/authchecker-service/
 USERNAME=bent
 URL=http://www.test.dk/test
-STREAMING_SERVER=rtmp://localhost:1937/kultur
-FILENAME=sample.mp4
+STREAMING_SERVER=rtmp://localhost:1937
+#FILENAME=sample.mp4
+FILENAME=rck_10mins.flv
 
 java -cp ../target/lib/*:../lib/jersey-1.3/jars/* dk.statsbiblioteket.doms.wowza.plugin.utilities.TicketTool $TICKET_SERVER $USERNAME $URL $STREAMING_SERVER $FILENAME
