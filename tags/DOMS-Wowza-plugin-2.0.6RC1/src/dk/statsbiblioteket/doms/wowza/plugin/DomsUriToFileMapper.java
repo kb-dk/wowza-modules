13bc3ce2ca5f0ccb8e275cd8bd20e0061faa15ed
Henning Böttger <heb@statsbiblioteket.dk>
2011-04-05 13:44:59 +0000

    Release candidate 2.0.6RC1

---
+++ tags/DOMS-Wowza-plugin-2.0.6RC1/src/dk/statsbiblioteket/doms/wowza/plugin/DomsUriToFileMapper.java
@@ -0,0 +1,222 @@
+/**
+ *
+ */
+package dk.statsbiblioteket.doms.wowza.plugin;
+
+import com.wowza.wms.client.IClient;
+import com.wowza.wms.logging.WMSLogger;
+import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.stream.IMediaStream;
+import com.wowza.wms.stream.IMediaStreamFileMapper;
+
+import dk.statsbiblioteket.doms.wowza.plugin.ticket.TicketChecker;
+import dk.statsbiblioteket.doms.wowza.plugin.ticket.TicketCheckerInterface;
+import dk.statsbiblioteket.doms.wowza.plugin.utilities.IllegallyFormattedQueryStringException;
+import dk.statsbiblioteket.doms.wowza.plugin.utilities.QueryUtil;
+
+import java.io.File;
+import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * This class decodes the query string of the URL by which we were called, and
+ * on the basis of this query string identifies the video to be played, and
+ * authorizes the player against the ticket checker.
+ *
+ * @author heb + jrg + mar + abr
+ */
+public class DomsUriToFileMapper implements IMediaStreamFileMapper {
+
+    private SimpleDateFormat sdf;
+
+    private String rickrollFilename;
+
+    private TicketCheckerInterface ticketChecker;
+
+    private String storageDir;
+    private IMediaStreamFileMapper backupMapper;
+
+    /**
+     * Constructor
+     *
+     * @param storageDir
+     */
+    public DomsUriToFileMapper(String storageDir,
+                               String format,
+                               String ticketInvalidErrorFile,
+                               String ticketCheckerLocation,
+                               IMediaStreamFileMapper backupMapper) {
+        this.backupMapper = backupMapper;
+        getLogger().info("Entered DomsUriToFileMapper(...)");
+        this.sdf = new SimpleDateFormat(format);
+        this.rickrollFilename = ticketInvalidErrorFile;
+        getLogger().info("Config value sdf: '" + sdf.toString() + "'");
+        getLogger().info("Config value rickrollFilename: '" + rickrollFilename
+                         + "'");
+        getLogger().info("Config value ticketCheckerLocation: '"
+                         + ticketCheckerLocation + "'");
+
+
+        this.storageDir = storageDir;
+
+        ticketChecker = new TicketChecker(ticketCheckerLocation);
+
+        getLogger().info("Creating mapper...");
+        getLogger().info("Creating mapper: StorageDir=" + storageDir);
+    }
+
+    public DomsUriToFileMapper(String storageDir, String format,
+			String ticketInvalidErrorFile, TicketCheckerInterface ticketChecker,
+			IMediaStreamFileMapper backupMapper) {
+        this.backupMapper = backupMapper;
+        getLogger().info("Entered DomsUriToFileMapper(...)");
+        this.sdf = new SimpleDateFormat(format);
+        this.rickrollFilename = ticketInvalidErrorFile;
+        getLogger().info("Config value sdf: '" + sdf.toString() + "'");
+        getLogger().info("Config value rickrollFilename: '" + rickrollFilename
+                         + "'");
+
+        this.storageDir = storageDir;
+
+        this.ticketChecker = ticketChecker;
+
+        getLogger().info("Creating mapper...");
+        getLogger().info("Creating mapper: StorageDir=" + storageDir);
+	}
+
+	/**
+     * Get the file that should be streamed. Extract its filename from query
+     * string, and check whether player is authorized to play it.
+     *
+     * @param stream The stream which we are about to provide to the client
+     * player.
+     * @return the file that should be streamed. In case the player was not
+     * authorized, an alternative video file, or just null, will be returned.
+     */
+    /* (non-Javadoc)
+      * @see com.wowza.wms.stream.IMediaStreamFileMapper#streamToFileForRead(
+      * com.wowza.wms.stream.IMediaStream)
+      *
+      * This method knows of the Wowza context, such as the content folder where
+      * the files are placed
+      */
+    @Override
+    public File streamToFileForRead(IMediaStream stream) {
+        getLogger().info("***Entered streamToFileForRead(IMediaStream stream)");
+        File fileToStream = null;
+
+        try {
+        	/*
+        	IClient client = stream.getClient();
+            if (client == null) {
+                getLogger().info("No client, returning ", stream);
+                return null;
+            }
+            String queryString;
+            queryString = URLDecoder.decode(client.getQueryStr(), "UTF-8");
+            String shardpid = Utils.extractShardID(queryString);
+            String shardurl = Utils.extractShardURL(queryString);
+            String ticket = Utils.extractTicket(queryString);
+            getLogger().info("queryString: '" + queryString + "'");
+            getLogger().info("Shardpid is "+shardpid);
+            getLogger().info("Shardurl is "+shardurl);
+            getLogger().info("Ticket is "+ticket);
+            if (ticketChecker.isTicketValid(ticket,shardurl,client.getIp())) {
+            	// Test purpose start
+            	getLogger().info("StorageDir: " + stream.getClient().getAppInstance().getStreamStorageDir());
+            	getLogger().info("Stream name: " + stream.getName());
+            	// Test purpose start
+                String filetype = stream.getExt();
+                if (filetype.isEmpty()){
+                    filetype = "flv";
+                }
+                String filename = Utils.extractShardID(queryString) + "." + filetype;
+                fileToStream = new File(storageDir + "/" + filename);
+                getLogger().info("filetype: '" + filetype + "'");
+                getLogger().info("filename: '" + filename + "'");
+                getLogger().info("file to stream: '" + fileToStream.getAbsolutePath() + "'");
+                getLogger().info("Got fileToStream");
+            } else {
+            	fileToStream = new File(rickrollFilename);
+            }
+        } catch (IllegallyFormattedQueryStringException e) {
+            getLogger().error("Query string has unexptected format. Parsing stopped with message: "+e.toString());
+            e.printStackTrace();
+            fileToStream = new File(rickrollFilename);*/
+        } catch (Exception e) {
+            // TODO better log level
+            getLogger().error("Unexpected error "+e.toString()+" occurred.");
+            e.printStackTrace();
+            fileToStream = new File(rickrollFilename);
+		}
+        getLogger().info("Playing file: " + fileToStream.getAbsolutePath());
+        return fileToStream;
+    }
+
+    /**
+     * Get the file that should be streamed.
+     *
+     * TODO javadoc
+     *
+     * @param stream
+     * @param name
+     * @param ext
+     * @param query
+     * @return
+     */
+    /* (non-Javadoc)
+      * @see com.wowza.wms.stream.IMediaStreamFileMapper#streamToFileForRead(
+      * com.wowza.wms.stream.IMediaStream, java.lang.String, java.lang.String,
+      * java.lang.String)
+      *
+      * This method knows of the Wowza context, such as the content folder where
+      * the files are placed
+      */
+    @Override
+    public File streamToFileForRead(IMediaStream stream, String name,
+                                    String ext, String query) {
+        getLogger().info("***Entered streamToFileForRead(IMediaStream stream, "
+                         + "String name, String ext, String query)");
+
+        return streamToFileForRead(stream);
+    }
+
+    @Override
+    public File streamToFileForWrite(IMediaStream arg0) {
+        getLogger().info("***Entered streamToFileForWrite(IMediaStream arg0)");
+        // TODO Auto-generated method stub
+        return backupMapper.streamToFileForWrite(arg0);
+    }
+
+    @Override
+    public File streamToFileForWrite(IMediaStream arg0, String arg1,
+                                     String arg2, String arg3) {
+        getLogger().info("***Entered streamToFileForWrite(IMediaStream arg0, "
+                         + "String arg1, String arg2, String arg3");
+        return backupMapper.streamToFileForWrite(arg0, arg1, arg2, arg3);
+    }
+
+    /**
+     * TODO javadoc
+     *
+     * @param dateString
+     * @throws java.text.ParseException
+     */
+    protected void validateStringAsDate(String dateString)
+            throws ParseException {
+        Date date = sdf.parse(dateString);
+        String dateReturnString = sdf.format(date);
+        if (!dateString.equals(dateReturnString)) {
+            throw new ParseException("Date is not valid. Read: " + dateString
+                                     + ". " + "Interpreted: " + dateReturnString, 0);
+        }
+    }
+
+    protected static WMSLogger getLogger()
+    {
+        return WMSLoggerFactory.getLogger(DomsUriToFileMapper.class);
+    }
+
+}
---
+++ tags/DOMS-Wowza-plugin-2.0.6RC1/src/dk/statsbiblioteket/doms/wowza/plugin/ticket/TicketTool.java
@@ -0,0 +1,123 @@
+package dk.statsbiblioteket.doms.wowza.plugin.ticket;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import com.wowza.wms.logging.WMSLogger;
+import com.wowza.wms.logging.WMSLoggerFactory;
+
+import dk.statsbiblioteket.util.Bytes;
+import dk.statsbiblioteket.util.Checksums;
+
+
+public class TicketTool implements TicketToolInterface {
+
+	private WMSLogger logger;
+    private WebResource restApi;
+
+	public TicketTool(String serviceURL, WMSLogger logger) {
+		super();
+        Client client = Client.create();
+        restApi = client.resource(serviceURL);
+        this.logger = logger;
+	}
+
+	@Override
+	public Ticket issueTicket(String username, String resource, List<TicketProperty> properties) {
+        try {                
+        	WebResource query = restApi
+            .path("/issueTicket")
+            .queryParam("username", username)
+            .queryParam("resource", resource);
+        	for (Iterator<TicketProperty> i = properties.iterator(); i.hasNext();) {
+        		TicketProperty prop = i.next();
+        		query = query.queryParam(prop.getName(), prop.getValue());
+        	}
+        	//System.out.println("Query parameters: " + query.toString());
+            Ticket ticketXml = query.post(Ticket.class);
+            return ticketXml;
+        }  catch (UniformInterfaceException e) {
+        	throw new RuntimeException("Unexpected event", e);
+        }
+	}
+
+	/* (non-Javadoc)
+	 * @see dk.statsbiblioteket.doms.wowza.plugin.utilities.TicketToolInterface#resolveTicket(java.lang.String)
+	 */
+	@Override
+	public Ticket resolveTicket(String ticketID) {
+		try {                
+			Ticket ticketXml = restApi
+			.path("/resolveTicket")
+			.queryParam("ID", ticketID)
+			.get(Ticket.class);
+			return ticketXml;
+
+		}  catch (UniformInterfaceException e) {
+			logger.warn("UniformInterfaceException occured. Ticket might be invalidated.", e);
+			return null;
+		}
+	}
+
+	public static void main(String args[]) {
+		String serviceURL = args[0];
+		String username = args[1];
+		String resource = args[2];
+		String organisationID = args[3];
+		String userID = args[4];
+		String channelID = args[5];
+		String programTitle = args[6];
+		String programStart = args[7];
+		
+		String streamingURL = args[8];
+		String filename = args[9];
+		String fileExtension = filename.substring(filename.length()-3);
+		String filenameWithoutExtension = filename.substring(0, filename.length()-4);
+
+		
+		List<TicketProperty> ticketProperties = new ArrayList<TicketProperty>();
+		ticketProperties.add(new TicketProperty("schacHomeOrganization", organisationID));
+		ticketProperties.add(new TicketProperty("eduPersonTargetedID", userID));
+		ticketProperties.add(new TicketProperty("metaChannelName", channelID));
+		ticketProperties.add(new TicketProperty("metaTitle", programTitle));
+		ticketProperties.add(new TicketProperty("metaDateTimeStart", programStart));
+		
+		System.out.println("---===<<< Ticket input parameters: >>>===---");
+		System.out.println("Ticket server    : " + serviceURL);
+		System.out.println("Username         : " + username);
+		System.out.println("Resource         : " + resource);
+		for	(Iterator<TicketProperty> i=ticketProperties.iterator();i.hasNext();) {
+			System.out.println("Property         : " + i.next().toString());
+		}
+		System.out.println("---===<<< Client input parameters: >>>===---");
+		System.out.println("Streaming server : " + streamingURL);
+		System.out.println("Filename         : " + filename);
+		System.out.println("Filename (no ext): " + filenameWithoutExtension);
+		System.out.println("File extension   : " + fileExtension);
+		System.out.println("");
+		System.out.print("Retrieving ticket...");
+		TicketToolInterface ticketTool = new TicketTool(serviceURL, WMSLoggerFactory.getLogger(TicketTool.class));
+		Ticket ticket = ticketTool.issueTicket(username, resource, ticketProperties);
+		System.out.println("[Success]");
+		System.out.println(ticket.toString());
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Input parameters for Wowza's test client <Wowza-install-dir>/examples/SimpleVideoStreaming/client/simplevideostreaming.html");
+		System.out.println("");
+		System.out.println("-[kultur]------------------------------------------------------------------------------------------------------------------");
+		System.out.println("Server : " + streamingURL + "/kultur?ticket=" + ticket.getID());
+		System.out.println("Stream : " + fileExtension + ":" + filename);
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("");
+		System.out.println("-[kultur_live]-------------------------------------------------------------------------------------------------------------");
+		System.out.println("Server : " + streamingURL + "/kultur_live?ticket=" + ticket.getID());
+		System.out.println("Stream : " + "stream" + ":" + Bytes.toHex(Checksums.md5(ticket.getID())) + ".stream");
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+		
+	}
+}
