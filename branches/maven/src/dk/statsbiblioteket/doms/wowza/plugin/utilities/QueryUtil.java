d05144b6dff921ba1fcd659565d963e000fec45f
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2012-10-03 15:51:22 +0000

    Create branch for mavenfication

---
+++ branches/maven/src/dk/statsbiblioteket/doms/wowza/plugin/utilities/QueryUtil.java
@@ -0,0 +1,36 @@
+package dk.statsbiblioteket.doms.wowza.plugin.utilities;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abr
+ * Date: Dec 2, 2010
+ * Time: 1:50:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QueryUtil {
+
+    // Create a pattern to match a correct query string
+    private static Pattern queryPattern = Pattern.compile(
+            "ticket=([^&]*)");
+
+    public static String extractTicketID(String queryString) throws IllegallyFormattedQueryStringException {
+        return extract(queryString,1);
+    }
+
+    private static String extract(String queryString, int group) throws IllegallyFormattedQueryStringException {
+        // Match
+        Matcher matcher = queryPattern.matcher(queryString);
+        boolean matchFound = matcher.find();
+        // Extract
+        if (matchFound) {
+            return matcher.group(group);
+        } else {
+            throw new IllegallyFormattedQueryStringException(
+            		"Query string '"+queryString+"' is not of the expected format.");
+        }
+
+    }
+}
