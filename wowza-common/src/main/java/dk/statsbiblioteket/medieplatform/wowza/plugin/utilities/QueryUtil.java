0ca21171e6e8fa922fae9c981d11cf8c1a15ca0a
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-04-17 14:11:03 +0200

    Simplyfy and loosely doc query util

--- wowza-common/src/main/java/dk/statsbiblioteket/medieplatform/wowza/plugin/utilities/QueryUtil.java
+++ wowza-common/src/main/java/dk/statsbiblioteket/medieplatform/wowza/plugin/utilities/QueryUtil.java
@@ -4,32 +4,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by IntelliJ IDEA.
- * User: abr
- * Date: Dec 2, 2010
- * Time: 1:50:03 PM
- * To change this template use File | Settings | File Templates.
+ * Extract ticket ID from query string
  */
 public class QueryUtil {
 
-    // Create a pattern to match a correct query string
+    // Create a pattern to match a correct query string. Note extra / is sometimes added and stripped here.
     private static Pattern queryPattern = Pattern.compile("ticket=([^/&]*)");
 
     public static String extractTicketID(String queryString) throws IllegallyFormattedQueryStringException {
-        return extract(queryString, 1);
-    }
-
-    private static String extract(String queryString, int group) throws IllegallyFormattedQueryStringException {
         // Match
         Matcher matcher = queryPattern.matcher(queryString);
-        boolean matchFound = matcher.find();
         // Extract
-        if (matchFound) {
-            return matcher.group(group);
+        if (matcher.find()) {
+            return matcher.group(1);
         } else {
             throw new IllegallyFormattedQueryStringException(
                     "Query string '" + queryString + "' is not of the expected format.");
         }
 
     }
+
 }
