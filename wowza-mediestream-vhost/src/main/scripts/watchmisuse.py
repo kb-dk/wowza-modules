cdac07bc43ea62db9b6a9eabbe5155c2940f5be2
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-06-04 11:11:47 +0200

    Add scripts for monitoring misuse

---
+++ wowza-mediestream-vhost/src/main/scripts/watchmisuse.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# Script that parses a log file from wowza and reports by mail users having streamed more than a given amount of streams
+# Usage:
+#   watchmisuse.py <logfile> <maxallowedstreams> <reportrecepient> <product>
+# Example:
+#   watchmisuse.py /home/wowza/logs/StreamingStat-$(date -d 'yesterday' +'%Y-%m-%d').log 20 kw@statsbiblioteket.dk,mvk@statsbiblioteket.dk "Mediestream STAGE"
+
+import json
+import sys
+import smtplib
+from email.mime.text import MIMEText
+
+count = {}
+maxcount = {}
+report = ''
+with open(sys.argv[1]) as f:
+    firstline = 1
+    for line in f:
+        if firstline:
+            firstline = 0
+            continue
+        parts = line.split(';')
+        action = parts[1]
+        attributes = json.loads(parts[3])
+        user = attributes.get('eduPersonPrincipalName')
+        if user:
+            user = str(user)
+            if action == 'PLAY':
+                count.setdefault(user, 0)
+                count[user] = count[user]+1;
+                maxcount.setdefault(user, 0)
+                maxcount[user] = max(count[user], maxcount[user])
+            elif action == 'STOP':
+                count[user] = count[user]-1;
+for c in maxcount:
+    if maxcount[c] >= int(sys.argv[2]):
+        report += 'User {} has streamed {} simultaneous streams.\n'.format(c, maxcount[c])
+if report:
+    msg = MIMEText(report)
+    msg['Subject'] = sys.argv[4] + ' misuse report'
+    msg['To'] = sys.argv[3];
+    msg['From'] = 'statsbiblioteket@statsbiblioteket.dk'
+    s = smtplib.SMTP('post.statsbiblioteket.dk')
+    s.sendmail('statsbiblioteket@statsbiblioteket.dk', sys.argv[3], msg.as_string())
+    s.quit()
---
+++ wowza-mediestream-vhost/src/main/scripts/watchplayer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# Script that parses a log file from wowza and reports by mail if streams have been played with different referrer than jwplayer
+# Usage:
+#   watchplayer.py <logfile> <reportrecepient>
+# Example:
+#   watchplayer.py /home/wowza/wowza/logs/wowzamediaserver_access.log.$(date -d 'yesterday' +'%Y-%m-%d') kw@statsbiblioteket.dk,mvk@statsbiblioteket.dk "Mediestream STAGE"
+
+import sys
+import smtplib
+from email.mime.text import MIMEText
+
+players = {}
+report = ''
+with open(sys.argv[1]) as f:
+    for line in f:
+        parts = line.split('\t')
+        if len(parts) < 18:
+            continue
+        action = parts[3]
+        if action == 'play':
+            player = parts[18]
+            players.setdefault(player, 0)
+            players[player] = players[player]+1
+for p in players:
+    if not(p.endswith('jwplayer.flash.swf')):
+        report += 'Streams have been played with non-approved player {} {} times.\n'.format(p, players[p])
+if report:
+    msg = MIMEText(report)
+    msg['Subject'] = sys.argv[3] + ' misuse report'
+    msg['To'] = sys.argv[2];
+    msg['From'] = 'statsbiblioteket@statsbiblioteket.dk'
+    s = smtplib.SMTP('post.statsbiblioteket.dk')
+    s.sendmail('statsbiblioteket@statsbiblioteket.dk', sys.argv[2], msg.as_string())
+    s.quit()
