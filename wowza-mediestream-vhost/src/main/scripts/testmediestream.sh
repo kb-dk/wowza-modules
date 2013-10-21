f552845d62bce554e6ea651d16be132d95936d7a
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-10-21 14:05:34 +0200

    More configurable, silent and executable

--- wowza-mediestream-vhost/src/main/scripts/testmediestream.sh
+++ wowza-mediestream-vhost/src/main/scripts/testmediestream.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TICKETCHECKERSERVICE=http://deneb.statsbiblioteket.dk:9651/ticket-system-service
+WOWZAURL=rtmp://thalassa:1935/mediestream
 PROGRAM=d68a0380-012a-4cd8-8e5b-37adf6c2d47f
-TICKET=$(curl "http://deneb.statsbiblioteket.dk:9651/ticket-system-service/tickets/issueTicket?id=doms_radioTVCollection:uuid:d68a0380-012a-4cd8-8e5b-37adf6c2d47f&ipAddress=$(hostname -I|head -n 1|tr -d ' ')&type=Stream&SBIPRoleMapper=inhouse"|cut -d\" -f4)
-ffprobe rtmp://thalassa:1935/mediestream?ticket=$TICKET/flv:$PROGRAM.flv >& /dev/null
+TICKET=$(curl "$TICKETCHECKERSERVICE/tickets/issueTicket?id=doms_radioTVCollection:uuid:d68a0380-012a-4cd8-8e5b-37adf6c2d47f&ipAddress=$(hostname -I|head -n 1|tr -d ' ')&type=Stream&SBIPRoleMapper=inhouse" 2> /dev/null|cut -d\" -f4)
+ffprobe $WOWZAURL?ticket=$TICKET/flv:$PROGRAM.flv >& /dev/null
