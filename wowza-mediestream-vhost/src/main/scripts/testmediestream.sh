8a97e9669d6e79a39e41d5db641b7bc87e2bba2e
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-10-23 19:22:14 +0200

    Update script with input from tgc

--- wowza-mediestream-vhost/src/main/scripts/testmediestream.sh
+++ wowza-mediestream-vhost/src/main/scripts/testmediestream.sh
@@ -3,10 +3,12 @@
 TICKETCHECKERSERVICE=http://deneb.statsbiblioteket.dk:9651/ticket-system-service
 WOWZAURL=rtmp://thalassa:1935/mediestream
 PROGRAM=d68a0380-012a-4cd8-8e5b-37adf6c2d47f
-TICKET=$(curl "$TICKETCHECKERSERVICE/tickets/issueTicket?id=doms_radioTVCollection:uuid:d68a0380-012a-4cd8-8e5b-37adf6c2d47f&ipAddress=$(hostname -I|cut -d ' ' -f1)&type=Stream&SBIPRoleMapper=inhouse" 2> /dev/null|cut -d\" -f4)
+IPADDR=$(hostname -i)
+TICKET=$(curl "$TICKETCHECKERSERVICE/tickets/issueTicket?id=doms_radioTVCollection:uuid:$PROGRAM&ipAddress=$IPADDR&type=Stream&SBIPRoleMapper=inhouse" 2> /dev/null|cut -d\" -f4)
 rtmpdump -q --stop=.1  -o /dev/null -r $WOWZAURL?ticket=$TICKET/flv:$PROGRAM.flv
 EXITCODE=$?
-if [ $EXITCODE == 2 ]
-then exit 0
-else exit $EXITCODE
+if [ $EXITCODE -eq 2 ]; then
+  exit 0
+else
+  exit $EXITCODE
 fi
