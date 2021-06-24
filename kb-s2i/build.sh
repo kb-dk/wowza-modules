086535c599e46387e279e42259e08b84e2a0fe28
Kim Christensen <ktc@kb.dk>
2021-06-24 15:30:31 +0200

    Initial openshift setup

---
+++ kb-s2i/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd /tmp/src
+
+tar xf "wowza-mediestream-vhost/target/wowza-mediestream-vhost-*-bundle.tar.gz"
+cp -rp -- /tmp/src/wowza-mediestream-vhost/target/wowza-mediestream-vhost-*/ /app/conf/wowza-mediestream-vhost
+
---
+++ kb-s2i/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp -- /tmp/src/conf/ocp/VHosts.xml "$CONF_DIR/VHosts.xml"
+ 
