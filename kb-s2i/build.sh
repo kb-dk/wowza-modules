430a7b12a964527887a97fbcee1f5cd66e26c500
Kim Christensen <ktc@kb.dk>
2021-06-25 09:23:29 +0200

    Extract the application and add setup steps

--- kb-s2i/build.sh
+++ kb-s2i/build.sh
@@ -2,6 +2,6 @@
 
 cd /tmp/src
 
-tar xf "/tmp/src/wowza-mediestream-vhost/target/wowza-mediestream-vhost-*-bundle.tar.gz"
-cp -rp -- /tmp/src/wowza-mediestream-vhost/target/wowza-mediestream-vhost-*/ /app/conf/wowza-mediestream-vhost
+tar xf /tmp/src/wowza-mediestream-vhost/target/wowza-mediestream-vhost-*-bundle.tar.gz -C /app/conf/
+mv /app/conf/wowza-mediestream-vhost-* /app/conf/wowza-mediestream-vhost
 
--- kb-s2i/deploy.sh
+++ kb-s2i/deploy.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 cp -- /tmp/src/conf/ocp/VHosts.xml "$CONF_DIR/VHosts.xml"
- 
+
+sed -i 's|<KeyStorePath>${user.home}/services/keystore.jks</KeyStorePath>|<KeyStorePath>/app/conf/keystore.jks</KeyStorePath>|' /app/conf/wowza-mediestream-vhost/conf/VHost.xml
+
+cp /app/conf/wowza-mediestream-vhost/conf/mediestream/wowza-module.properties /app/conf/mediestream-wowza-module.properties
+ln -sf /app/conf/mediestream-wowza-module.properties  /app/conf/wowza-mediestream-vhost/conf/mediestream/wowza-module.properties
+ln -sf /app/conf/mediestream-wowza-module.properties  /app/conf/wowza-mediestream-vhost/conf/mediestreamapple/wowza-module.properties
+chmod -R g+w /app/conf
+
+mkdir -p /app/content/mediestream/{doms,kuana,kuanaradio}
+ln -s /app/content/mediestream/doms /app/conf/streamingContent/doms
+ln -s /app/content/mediestream/kuana /app/conf/streamingContent/kuana
+ln -s /app/content/mediestream/kuanaradio /app/conf/streamingContent/kuanaradio
