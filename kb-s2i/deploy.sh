29acd72f5d638a1a5abe6f2ae8dcf0f38e2b748a
Kim Christensen <ktc@kb.dk>
2021-06-25 09:43:36 +0200

    Fix paths for symlinks etc

--- kb-s2i/deploy.sh
+++ kb-s2i/deploy.sh
@@ -4,12 +4,12 @@ cp -- /tmp/src/conf/ocp/VHosts.xml "$CONF_DIR/VHosts.xml"
 
 sed -i 's|<KeyStorePath>${user.home}/services/keystore.jks</KeyStorePath>|<KeyStorePath>/app/conf/keystore.jks</KeyStorePath>|' /app/conf/wowza-mediestream-vhost/conf/VHost.xml
 
-cp /app/conf/wowza-mediestream-vhost/conf/mediestream/wowza-module.properties /app/conf/mediestream-wowza-module.properties
-ln -sf /app/conf/mediestream-wowza-module.properties  /app/conf/wowza-mediestream-vhost/conf/mediestream/wowza-module.properties
-ln -sf /app/conf/mediestream-wowza-module.properties  /app/conf/wowza-mediestream-vhost/conf/mediestreamapple/wowza-module.properties
+cp /app/conf/wowza-mediestream-vhost/conf/mediestream/wowza-modules.properties /app/conf/mediestream-wowza-modules.properties
+ln -sf /app/conf/mediestream-wowza-modules.properties  /app/conf/wowza-mediestream-vhost/conf/mediestream/wowza-modules.properties
+ln -sf /app/conf/mediestream-wowza-modules.properties  /app/conf/wowza-mediestream-vhost/conf/mediestreamapple/wowza-modules.properties
 chmod -R g+w /app/conf
 
 mkdir -p /app/content/mediestream/{doms,kuana,kuanaradio}
-ln -s /app/content/mediestream/doms /app/conf/streamingContent/doms
-ln -s /app/content/mediestream/kuana /app/conf/streamingContent/kuana
-ln -s /app/content/mediestream/kuanaradio /app/conf/streamingContent/kuanaradio
+ln -s /app/content/mediestream/doms /app/conf/wowza-mediestream-vhost/streamingContent/doms
+ln -s /app/content/mediestream/kuana /app/conf/wowza-mediestream-vhost/streamingContent/kuana
+ln -s /app/content/mediestream/kuanaradio /app/conf/wowza-mediestream-vhost/streamingContent/kuanaradio
